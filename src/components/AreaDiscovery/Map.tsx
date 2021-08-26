bd12beb0953a4264d077b39b66c1f671d62ef6c6
Maarten Schumacher <maartenschumacher@gmail.com>
2021-08-26 11:19:19 +0200

    use custom marker

--- src/components/AreaDiscovery/Map.tsx
+++ src/components/AreaDiscovery/Map.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import MapView, {Marker} from 'react-native-maps';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import landmarks from './londonLandmarks.json';
 
 const LONDON_REGION = {
@@ -14,7 +15,9 @@ export const Map = () => (
     style={{width: '100%', height: '100%'}}
     initialRegion={LONDON_REGION}>
     {landmarks.map(landmark => (
-      <Marker key={landmark.id} coordinate={landmark.latlng} />
+      <Marker key={landmark.id} coordinate={landmark.latlng}>
+        <Icon name="map-marker" size={44} color="grey" />
+      </Marker>
     ))}
   </MapView>
 );
