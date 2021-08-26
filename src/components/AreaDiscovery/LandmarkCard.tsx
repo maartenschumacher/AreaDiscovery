8ac34ff7a4a374f182f0415ff0147e61d95815ce
Maarten Schumacher <maartenschumacher@gmail.com>
2021-08-26 12:49:04 +0200

    add card title

--- src/components/AreaDiscovery/LandmarkCard.tsx
+++ src/components/AreaDiscovery/LandmarkCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Image, StyleSheet, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 import {Landmark} from 'types/Landmark';
 
 type Props = {
@@ -10,6 +10,7 @@ export const LandmarkCard: React.FC<Props> = ({landmark}) => {
   return (
     <View style={styles.container}>
       <Image source={{uri: landmark.image}} style={styles.image} />
+      <Text style={styles.title}>{landmark.name}</Text>
     </View>
   );
 };
@@ -22,9 +23,17 @@ const styles = StyleSheet.create({
     height: 150,
     marginTop: 20,
     marginLeft: 20,
+    justifyContent: 'flex-end',
   },
   image: {
+    position: 'absolute',
     width: '100%',
     height: '100%',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'white',
+    margin: 10,
   },
 });
