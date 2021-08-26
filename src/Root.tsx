b83e7d2f11cda804650cd55660d99cfb65e40608
Maarten Schumacher <maartenschumacher@gmail.com>
2021-08-26 17:30:14 +0200

    set up react navigation

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import {AreaDiscovery} from './components/AreaDiscovery';
 import {Provider} from 'react-redux';
 import {store} from './store';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+
+const Stack = createStackNavigator();
 
 export const Root = () => {
   return (
-    <Provider store={store}>
-      <AreaDiscovery />
-    </Provider>
+    <NavigationContainer>
+      <Provider store={store}>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="AreaDiscovery" component={AreaDiscovery} />
+        </Stack.Navigator>
+      </Provider>
+    </NavigationContainer>
   );
 };
