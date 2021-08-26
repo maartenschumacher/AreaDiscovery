ad2c4565dc3405c088cbc803a26b3e9d6cc6550c
Maarten Schumacher <maartenschumacher@gmail.com>
2021-08-26 10:45:33 +0200

    focus map on london

--- src/components/AreaDiscovery/Map.tsx
+++ src/components/AreaDiscovery/Map.tsx
@@ -1,4 +1,16 @@
 import React from 'react';
 import MapView from 'react-native-maps';
 
-export const Map = () => <MapView style={{width: '100%', height: '100%'}} />;
+const LONDON_REGION = {
+  latitude: 51.475772098209575,
+  latitudeDelta: 0.3580543927585893,
+  longitude: -0.0887239031507363,
+  longitudeDelta: 0.2938382496812153,
+};
+
+export const Map = () => (
+  <MapView
+    style={{width: '100%', height: '100%'}}
+    initialRegion={LONDON_REGION}
+  />
+);
