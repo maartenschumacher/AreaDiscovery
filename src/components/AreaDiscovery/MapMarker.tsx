46306669f8c86040197bdeb1c340d1f406d5f855
Maarten Schumacher <maartenschumacher@gmail.com>
2021-08-27 09:27:29 +0200

    fix: glitchy marker button behavior

--- src/components/AreaDiscovery/MapMarker.tsx
+++ src/components/AreaDiscovery/MapMarker.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, TouchableOpacity, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {Marker} from 'react-native-maps';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {useSelector} from 'react-redux';
@@ -19,20 +19,18 @@ export const MapMarker: React.FC<Props> = ({landmark, isSelected, onPress}) => {
   );
 
   return (
-    <Marker key={landmark.id} coordinate={landmark.latlng}>
-      <TouchableOpacity onPress={onPress}>
-        <Icon
-          name="map-marker"
-          size={44}
-          color={isSelected ? 'blue' : 'grey'}
-          testID="marker"
-        />
-        {isFavourite ? (
-          <View style={styles.heart} testID="marker-heart">
-            <Icon name="heart" size={15} color="red" />
-          </View>
-        ) : null}
-      </TouchableOpacity>
+    <Marker key={landmark.id} coordinate={landmark.latlng} onPress={onPress}>
+      <Icon
+        name="map-marker"
+        size={44}
+        color={isSelected ? 'blue' : 'grey'}
+        testID="marker"
+      />
+      {isFavourite ? (
+        <View style={styles.heart} testID="marker-heart">
+          <Icon name="heart" size={15} color="red" />
+        </View>
+      ) : null}
     </Marker>
   );
 };
